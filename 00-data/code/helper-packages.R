a650fca0be6dbbee42ef652948724dd618bb9f38
jil028 <85192788+jil028@users.noreply.github.com>
2022-11-19 21:01:06 -0800

    Create helper-packages.R

---
+++ 00-data/code/helper-packages.R
@@ -0,0 +1,33 @@
+# load data
+library(rio)
+library(labelled)
+
+# glue strings
+library(glue)
+
+# data cleaning
+library(tidyverse)
+library(lubridate)
+library(janitor)
+
+# data analysis
+library(lfe)
+
+# visualization
+library(ggpubr)
+library(cowplot)
+library(forestplot)
+library(ggmap)
+library(googleway)
+
+# geographical analysis
+library(sf)
+library(sp)
+library(leaflet)
+
+# python
+library(reticulate)
+
+# model summary/latex
+library(kableExtra)
+library(modelsummary)
