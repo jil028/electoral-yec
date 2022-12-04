c2e38fe6d5113cf2434b7b05766ed0b3d5e83826
jil028 <85192788+jil028@users.noreply.github.com>
2022-12-04 15:25:26 -0800

    Update geocode_openrice.py

--- 00-data/code/data-collection/restaurant-data/geocode_openrice.py
+++ 00-data/code/data-collection/restaurant-data/geocode_openrice.py
@@ -9,16 +9,20 @@ import requests
 openrice_raw = pd.read_csv('../clean_openrice_not_geocoded.csv', index_col = [0])
 openrice_raw.head()
 
+# Enter API key
 gmaps = googlemaps.Client(key = 'Enter your Google Maps API key here')
 
 # function to geocode locations
 def glookup(names):
+	# initialize empty lists
 	geocode_result = []
 	coordinates = []
+	# loop in each restaurant's name to geocode
 	for i in range(len(names)):
 		geocode_result.append(gmaps.geocode(names[i]))
 		location = geocode_result[i][0]['geometry']['location'] if bool(geocode_result[i]) else None
-		print(i)
+		# showing the progress
+		print(i) 
 		coordinates.append([location['lat'], location['lng']]) if location is not None else coordinates.append([0,0])
 	return coordinates
 
