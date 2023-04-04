ae41601eac6770a5aa14049f2720a8a769633175
jil028 <85192788+jil028@users.noreply.github.com>
2023-04-03 23:03:26 -0700

    Add files via upload

---
+++ 01-outputs/helper-packages.R
@@ -0,0 +1,33 @@
+# load data
+library(rio)
+library(labelled)
+
+# glue strings
+library(glue)
+
+# data cleaning
+library(tidyverse)
+library(lubridate)
+library(janitor)
+
+# data analysis
+library(lfe)
+library(broom)
+
+# visualization
+library(ggpubr)
+library(cowplot)
+library(forestplot)
+library(wesanderson)
+
+# geographical analysis
+library(sf)
+library(sp)
+library(leaflet)
+library(ggmap)
+library(googleway)
+library(lwgeom)
+
+# model summary/latex
+library(kableExtra)
+library(modelsummary)
